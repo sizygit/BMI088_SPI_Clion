a87234f1715d8c5a6d4fa96792b6b384071221af
Your Name <2432317262@qq.com>
2021-07-25 18:47:51 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/DependInfo.cmake
@@ -0,0 +1,90 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Core/Startup/startup_stm32f407ighx.s" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Core/Startup/startup_stm32f407ighx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../component/BMI088"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Core/Src/dma.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Core/Src/dma.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Core/Src/gpio.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Core/Src/gpio.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Core/Src/main.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Core/Src/main.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Core/Src/spi.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Core/Src/spi.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Core/Src/stm32f4xx_hal_msp.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Core/Src/stm32f4xx_it.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Core/Src/syscalls.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Core/Src/syscalls.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Core/Src/sysmem.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Core/Src/sysmem.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Core/Src/system_stm32f4xx.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Core/Src/tim.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Core/Src/tim.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Core/Src/usart.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Core/Src/usart.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/component/BMI088/BMI088Middleware.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/component/BMI088/BMI088Middleware.c.obj"
+  "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/component/BMI088/BMI088driver.c" "/home/szy/STM32-files/BMI088/BMI_088_SPI_Clion/cmake-build-debug/CMakeFiles/BMI_088_SPI_Clion.elf.dir/component/BMI088/BMI088driver.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../component/BMI088"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ component/BMI088/struct_typedef.h
@@ -0,0 +1,16 @@
+#ifndef STRUCT_TYPEDEF_H
+#define STRUCT_TYPEDEF_H
+
+
+/* exact-width unsigned integer types */
+typedef float float_32;
+typedef double double_64;
+
+
+
+
+
+
+
+
+#endif
